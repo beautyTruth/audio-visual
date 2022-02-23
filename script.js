2019c511388e75bfc2263f8c27b1c5b75f0f1549
Phil <happy.melodist@gmail.com>
2022-02-22 21:43:29 -0600

    complete project by adding aclassy fart sound to the animation

--- script.js
+++ script.js
@@ -11,40 +11,49 @@ class Explosion {
     this.spriteHeight = 179;
     this.width = this.spriteWidth * 0.7;
     this.height = this.spriteHeight * 0.7;
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = "boom.png";
     this.frame = 0;
     this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = "fart.mp3";
   }
   update() {
+    if (this.frame === 0) this.sound.play();
     ++this.timer;
     if (this.timer % 10 === 0) {
       ++this.frame;
     }
   }
   draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width * 0.5,
+      0 - this.height * 0.5,
       this.width,
       this.height
     );
+    ctx.restore();
   }
 }
 
 window.addEventListener("click", (e) => {
   createAnimation(e);
 });
-window.addEventListener("mousemove", (e) => {
-  createAnimation(e);
-});
+
+// window.addEventListener("mousemove", (e) => {
+//   createAnimation(e);
+// });
 
 function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
