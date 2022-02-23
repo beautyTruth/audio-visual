ad6d4f66b81b2d8212c789068bf233db72c85c05
Phil <happy.melodist@gmail.com>
2022-02-22 21:04:50 -0600

    complete project through the creation of the mousemove event listener at approx 20:38

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 700;
 const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.spriteWidth = 200;
     this.spriteHeight = 179;
-    this.width = this.spriteWidth * 0.5;
-    this.height = this.spriteHeight * 0.5;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.x = x - this.width / 2;
+    this.y = y - this.height / 2;
     this.image = new Image();
     this.image.src = "boom.png";
     this.frame = 0;
+    this.timer = 0;
   }
   update() {
-    ++this.frame;
+    ++this.timer;
+    if (this.timer % 10 === 0) {
+      ++this.frame;
+    }
   }
   draw() {
     ctx.drawImage(
@@ -35,6 +40,29 @@ class Explosion {
 }
 
 window.addEventListener("click", (e) => {
-  console.log(e);
-  // ctx.fillRect(x.y.width.height);
+  createAnimation(e);
 });
+window.addEventListener("mousemove", (e) => {
+  createAnimation(e);
+});
+
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+    if (explosions[i].frame > 5) {
+      explosions.splice(i, 1);
+      --i;
+    }
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
